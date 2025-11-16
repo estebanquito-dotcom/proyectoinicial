fd47a891a7243aace4e17e807f921c4dea82d43a
esteb4n_quito <em016822@gmail.com>
2025-11-15 19:51:52 -0500

    feature de cambios rama2

--- src/App.java
+++ src/App.java
@@ -3,6 +3,16 @@ public class App {
         System.out.println("Hello, world estebanquito");
         System.out.println("segunda instruccion");
         System.out.println("tercera instruccion");
+        main2();
+    }
+
+    public static void main2() {
+        System.out.println("modificacion de la rama original");
     }
 }
+
+
+
+
+
 
