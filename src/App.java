6860eabd70dcf6a166edd51b8c5b7e2f8dae6b80
esteb4n_quito <em016822@gmail.com>
2025-11-15 19:19:25 -0500

    arquetipo del proyecto

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, world estebanquito");
+        System.out.println("segunda instruccion");
+        System.out.println("tercera instruccion");
+    }
+}
+
